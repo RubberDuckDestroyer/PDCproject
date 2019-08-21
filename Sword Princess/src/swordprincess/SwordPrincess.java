efa2fe19d0ac00abc203b9826cf33021bf7e3cae
Hitarth Asrani <hitarthasrani@gmail.com>
2019-08-21 19:27:44 +1200

    Added text reader

--- Sword Princess/src/swordprincess/SwordPrincess.java
+++ Sword Princess/src/swordprincess/SwordPrincess.java
@@ -5,19 +5,61 @@
  */
 package swordprincess;
 
+import java.io.*;
+
 /**
- *
- * @author Patricia
+ * This class is the main game class for the Sword Princess Game.
+ * 
+ * @author Patricia Virgen and Hitarth Asrani
  */
 public class SwordPrincess {
 
+    private boolean gameInProgress;
+    private Mode gameMode;
     /**
-     * @param args the command line arguments
+     * Enum Mode stores game modes available: Battle and Adventure
+     */ 
+    public enum Mode 
+    {
+        battle, adventure;
+    }
+    
+    /**
+     * Play method
      */
+    
+    private void play()
+    {
+        System.out.println("Playing game!");
+        /*
+        this.player = player;
+        //Enter room
+        while(gameInProgess)
+        {
+            switch (this.gameMode)
+            {
+                case adevnture: 
+                        break;
+                case battle: 
+                    break;
+            }
+        }
+        */
+    }
+    /**
+    *  Main method
+    */
     public static void main(String[] args) {
         // TODO code application logic here
         
         System.out.println("Hello! It's Sword Princress!");
+        System.out.println(System.getProperty("user.dir"));
+        System.out.println(new File("Data\\worlddata.txt").getAbsoluteFile());
+        SwordPrincess game = WorldGenerator.newWorld();
+        //TODO: Initialise Player
+        //TODO: Play game/ Main game loop method!
+        game.play();
+        
     }
     
 }
--- Sword Princess/src/swordprincess/WorldGenerator.java
+++ Sword Princess/src/swordprincess/WorldGenerator.java
@@ -4,11 +4,86 @@
  * and open the template in the editor.
  */
 package swordprincess;
+import characters.Monster;
+import entities.Sword;
+import gamemap.Forest;
+import java.io.*;
+import java.util.*;
 
 /**
- *
- * @author hitar
+ * This class is the Data reader class for the main game.
+ * @author Patricia Virgen and Hitarth Asrani
  */
-public class WorldGenerator {
-    
+public class WorldGenerator 
+{
+    private static final String worldDataPath = "Data\\worlddata.txt";
+    private static final boolean outputToConsole = true;
+    // Enum for world data tags used.
+    public enum WorldDataTag
+    {
+        rooms, desc;
+        public static WorldDataTag readLine(String line)
+        {
+            line = line.replace("$","").toLowerCase();
+            return WorldDataTag.valueOf(line);
+        }
+    }    
+        //TODO: Logger
+        private static void log(String text)
+        {
+            if(outputToConsole)
+            {
+                System.out.println(text);
+            }
+        }
+        /*
+        private static Sword parseWeaponData(String data)
+        {
+        }
+        
+        private static Forest generateForest()
+        {
+        }
+        
+        private static Forest generateRoom()
+        {
+        }
+        
+        private static Monster parseMonsterData(String data)
+        {
+        }
+        */
+        private static SwordPrincess loadWorld(String path) throws IOException
+        {
+            Scanner fileReader = new Scanner(new File(path));
+            
+            log("Loading game data from: "+ path);
+            log("meta tag: "+ fileReader.nextLine());
+            int numberOfForests = new Integer(fileReader.nextLine());
+            log("Number of forests: "+ numberOfForests);
+            while(!fileReader.hasNextLine())
+            {
+                fileReader.nextLine();
+            }
+                fileReader.close();
+            
+            
+            return null;
+        }
+        public static SwordPrincess newWorld()
+        {
+            String path = worldDataPath;
+            try
+            {
+                return WorldGenerator.loadWorld(path);
+            } catch (IOException ioe)
+            {
+                System.err.println("ERROR: File not found! Path: "+ path );
+                System.out.println("Searching in: " + new File(path).getAbsolutePath());
+                ioe.printStackTrace();
+            }
+            
+            // Might need to change this to return read world based on params
+            return null;
+        }
 }
